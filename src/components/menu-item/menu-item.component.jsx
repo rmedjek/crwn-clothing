f46b2f15b8e69b66062d454202e667493d028381
Red Medjek <red.medjek@blueyonder.com>
2020-08-08 04:21:32 -0400

    test commit
    See Also: ISL-XXXX

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
+      style={{ backgroundImage: `url(${imageUrl})`}}
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
