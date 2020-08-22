95c322bbcaa74ab4c3db128876f27af2be788317
red-medjek <medjekred@gmail.com>
2020-08-22 06:00:55 -0400

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components wtih new ux flow for directory/shop

---
+++ src/redux/shop/dist/shop.selector.dev.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.selectCollection = exports.selectCollectionsForPreview = exports.selectCollections = void 0;
+
+var _reselect = require("reselect");
+
+var selectShop = function selectShop(state) {
+  return state.shop;
+};
+
+var selectCollections = (0, _reselect.createSelector)([selectShop], function (shop) {
+  return shop.collections;
+});
+exports.selectCollections = selectCollections;
+var selectCollectionsForPreview = (0, _reselect.createSelector)([selectCollections], function (collections) {
+  return Object.keys(collections).map(function (key) {
+    return collections[key];
+  });
+});
+exports.selectCollectionsForPreview = selectCollectionsForPreview;
+
+var selectCollection = function selectCollection(collectionUrlParam) {
+  return (0, _reselect.createSelector)([selectCollections], function (collections) {
+    return collections[collectionUrlParam];
+  });
+};
+
+exports.selectCollection = selectCollection;
