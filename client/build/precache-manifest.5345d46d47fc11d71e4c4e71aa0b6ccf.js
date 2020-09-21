33ec90906a0734c29080ee845ecd74ec81a9df06
red-medjek <medjekred@gmail.com>
2020-09-21 03:36:51 -0400

    separting our cra app into client folder, added express server to help handle payments

---
+++ client/build/precache-manifest.5345d46d47fc11d71e4c4e71aa0b6ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3c5871e478de19b12293347d2c920a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d17b5847d209b5d02ad",
+    "url": "/static/css/main.a41b2f61.chunk.css"
+  },
+  {
+    "revision": "54742a37892833cb7757",
+    "url": "/static/js/2.0ec3e34d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ec3e34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d17b5847d209b5d02ad",
+    "url": "/static/js/main.6e8ec478.chunk.js"
+  },
+  {
+    "revision": "6bedc7d4e72b37a14961",
+    "url": "/static/js/runtime-main.059467c3.js"
+  }
+]);
